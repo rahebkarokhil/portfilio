396ddffa0cc45a71dde6e1f5485c1cac41146b25
rahebkarokhil <rahebkarokhil@gmail.com>
2026-01-10 21:23:06 +0430

    Fix image paths for Vercel

--- src/Compononts/Home/Project/ProjectData.js
+++ src/Compononts/Home/Project/ProjectData.js
@@ -3,44 +3,52 @@ import Play from '../../../assets/project/play-min.png'
 import shoper from '../../../assets/project/shoper-min.png'
 import suntower from '../../../assets/project/suntower-min.png'
 import Tailwind from '../../../assets/project/tailwind-min.png'
+import CEO from '../../../assets/project/CEO.png'
 
 export const projects = [
   {
     id: 1,
-    title: "Shoper",
+    title: "SHOPPER",
     image: shoper,
     github: "https://github.com/rahebkarokhil/ShoppereCommerce-Temlate-Byraheb",
     vercel: "https://shoppere-commerce-temlate-busniss.vercel.app/",
   },
   {
     id: 2,
-    title: "Play",
-    image: Play,
-    github: "https://github.com/rahebkarokhil/Play",
-    vercel: "https://play-six-sage.vercel.app/",
+    title: "CEO CLUB AFGHANISTAN",
+    image: CEO,
+    github: "https://github.com/rahebkarokhil/CEO-club-Afghanistan",
+    vercel: "https://ceo-club-afghanistan-lemon.vercel.app/",
   },
   {
     id: 3,
-    title: "Suntower",
+    title: "SUNTOWER",
     image: suntower,
     github: "https://github.com/rahebkarokhil/Suntower",
     vercel: "https://suntowr05.vercel.app/",
   },
   {
     id: 4,
-    title: "Adi-Plus medical Complix",
+    title: "ADI PIUS MEDECAL COMPLIX",
     image: adi,
     github: "https://github.com/rahebkarokhil/adi-medical",
     vercel: "https://adi-plus-medical-complix.vercel.app",
   },
   {
     id: 5,
-    title: "Tailwind CSS",
+    title: "TAILWIND CSS",
     image: Tailwind,
     github: "https://github.com/rahebkarokhil/Tailwind-Css",
     vercel: "https://tailwind-css-seven-lime.vercel.app",
   },
   
   
+   {
+    id: 6,
+    title: "PLAY",
+    image: Play,
+    github: "https://github.com/rahebkarokhil/Play",
+    vercel: "https://play-six-sage.vercel.app/",
+  },
     
 ];
